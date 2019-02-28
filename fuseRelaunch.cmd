a6367c401dc7af859fd376f24f1213d24955ae3b
rtphan <34082896+rtphan@users.noreply.github.com>
2019-02-28 11:34:53 -0800

    Revert "Implement clock divider without pause or reset"

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ise/CS152A-lab4/clock_divider_tb_isim_beh.exe" -prj "/home/ise/CS152A-lab4/clock_divider_tb_beh.prj" "work.clock_divider_tb" "work.glbl" 
